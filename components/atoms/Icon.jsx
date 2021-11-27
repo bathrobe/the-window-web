40ff93c37f6a3b9f30187980d5cdba83115e25d4
Joe <joe@seph.news>
2021-11-26 22:14:22 -0500

    adding headers

--- components/atoms/Icon.jsx
+++ components/atoms/Icon.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import {BsFillCalendarEventFill} from "react-icons/bs"
-import {GoGlobe} from 'react-icons/go'
-import {RiArticleFill} from 'react-icons/ri'
-export default function Icon({type}) {
-    let icon;
-    switch(type){
-        case "calendar":
-            icon = <BsFillCalendarEventFill/>
-        break;
-        case "globe":
-            icon = <GoGlobe/>
-        break;
-        case "article":
-            icon = <RiArticleFill/>
-        break;
-    }
+import React from "react";
+import { BsFillCalendarEventFill } from "react-icons/bs";
+import { GoGlobe } from "react-icons/go";
+import { RiArticleFill } from "react-icons/ri";
+import { BsSearch } from "react-icons/bs";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IconContext } from "react-icons";
 
-    return (
-        <>{icon}</>
-    )
+export default function Icon({ type }) {
+  let icon;
+  switch (type) {
+    case "calendar":
+      icon = <BsFillCalendarEventFill />;
+      break;
+    case "globe":
+      icon = <GoGlobe />;
+      break;
+    case "article":
+      icon = <RiArticleFill />;
+      break;
+    case "search":
+      icon = <BsSearch />;
+      break;
+    case "menu":
+      icon = <HiOutlineMenuAlt3 />;
+      break;
+  }
+
+  return (
+    <IconContext.Provider value={{ className: "text-gray-400" }}>
+      {icon}
+    </IconContext.Provider>
+  );
 }
--- components/atoms/ImageAtom.jsx
+++ components/atoms/ImageAtom.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import Image from 'next/image'
-import urlFor from "../../lib/sanity/urlFor"
+import React from "react";
+import Image from "next/image";
+import urlFor from "../../lib/sanity/urlFor";
 
-export default function ImageAtom({url, size, orientation}) {
-    let w;
-    let h;
-    switch(orientation){
-        case "square":
-            switch(size){
-                case "s":            
-                    w = 125
-                    h = 125
-                break;
-                case "m":
-                    w = 225
-                    h = 225
-                break;
-            }
-        break;
-        case "landscape":
-            w = 700
-            h = 525
-        break;
-    }
-    let src = urlFor(url).fit("crop").crop("entropy").width(w).height(h).url()
-    let srcBlur = urlFor(url).fit("crop").crop("entropy").width(w).height(h).blur(75).url()
-    return (
-        <Image src={src} placeholder="blur" blurDataURL={srcBlur} width={w} height={h}/>
-    )
+export default function ImageAtom({ url, size, orientation }) {
+  let w;
+  let h;
+  switch (orientation) {
+    case "square":
+      switch (size) {
+        case "s":
+          w = 125;
+          h = 125;
+          break;
+        case "m":
+          w = 225;
+          h = 225;
+          break;
+      }
+      break;
+    case "landscape":
+      w = 700;
+      h = 525;
+      break;
+    case "portrait":
+      w = 250;
+      h = 100;
+      break;
+  }
+  let src = urlFor(url).fit("crop").crop("entropy").width(w).height(h).url();
+  let srcBlur = urlFor(url)
+    .fit("crop")
+    .crop("entropy")
+    .width(w)
+    .height(h)
+    .blur(75)
+    .url();
+  return (
+    <Image
+      src={src}
+      placeholder="blur"
+      blurDataURL={srcBlur}
+      width={w}
+      height={h}
+    />
+  );
 }
